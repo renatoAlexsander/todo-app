2242420d141c0e0a8f31100b04714096677e33bf
renatoAlexsander <renato.alexsander13@gmail.com>
2020-04-29 19:18:11 -0300

    Add component todoItem

--- App.js
+++ App.js
@@ -1,13 +1,33 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 import Header from "./src/components/header";
+import TodoItem from "./src/components/todoItem";
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    { id: 1, text: "make a coffee" },
+    { id: 2, text: "watch some film" },
+    { id: 3, text: "wake up early" },
+  ]);
+
+  const pressHandler = (id) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((t) => t.id !== id);
+    });
+  };
+
   return (
     <View style={styles.container}>
       <Header />
       <View style={styles.content}>
-        <View style={styles.list}></View>
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => (
+              <TodoItem item={item} pressHandler={pressHandler}></TodoItem>
+            )}
+          ></FlatList>
+        </View>
       </View>
     </View>
   );
